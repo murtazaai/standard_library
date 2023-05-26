098dc0c52b8c817ce616bbd75eac32da6fbc1eb8
murtazaii <murtazalimtiaz@gmail.com>
2023-05-26 04:25:05 -0400

    Domain name resolution.

--- src/w_n/mod.rs
+++ src/w_n/mod.rs
@@ -1,4 +1,3 @@
-/***
-    World Wide Web = W3
-*/
 /// Can you entertain the host? (https://www.icann.org)
+/// History of spider(https://australian.museum/learn/animals/spiders/spider-origins/)
+/// World Wide Web = W3
